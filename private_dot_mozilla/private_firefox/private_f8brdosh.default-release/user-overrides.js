8ff99b1a2d05a2ce326e690f3c173d33b31fee94
AAGaming00 <aa@bigdumb.gq>
2021-06-23 12:31:42 -0400

    Update private_dot_config/private_discordcanary/settings.json Update private_dot_mozilla/private_firefox/private_f8brdosh.default-release/user-overrides.js

--- private_dot_mozilla/private_firefox/private_f8brdosh.default-release/user-overrides.js
+++ private_dot_mozilla/private_firefox/private_f8brdosh.default-release/user-overrides.js
@@ -7,7 +7,11 @@ user_pref("extensions.webcompat-reporter.enabled", true); // [DEFAULT: false]
 user_pref("network.dns.disableIPv6", false);
 user_pref("keyword.enabled", true);
 user_pref("browser.search.suggest.enabled", true);
+user_pref("browser.urlbar.autoFill", true);
+user_pref("browser.urlbar.suggest.history", true);
+user_pref("browser.urlbar.suggest.bookmark", true);
 user_pref("browser.urlbar.suggest.searches", true);
+user_pref("browser.urlbar.maxRichResults", 10);
 user_pref("browser.cache.disk.enable", true);
 user_pref("browser.sessionstore.privacy_level", 0);
 user_pref("browser.sessionstore.interval", 15000);
@@ -33,7 +37,7 @@ user_pref("javascript.options.asmjs", true);
 user_pref("javascript.options.wasm", true);
 user_pref("media.navigator.enabled", true);
 user_pref("dom.webaudio.enabled", true);
-user_pref("accessibility.force_disabled", 1); // this probably breaks KDE stuff
+user_pref("accessibility.force_disabled", 0); // this probably breaks KDE stuff
 user_pref("browser.pagethumbnails.capturing_disabled", false); // [HIDDEN PREF]
 user_pref("browser.display.use_system_colors", true); // [DEFAULT: false]
 user_pref("privacy.sanitize.sanitizeOnShutdown", false);
@@ -43,3 +47,14 @@ user_pref("privacy.resistFingerprinting.letterboxing", false); // [HIDDEN PREF]
 user_pref("general.useragent.override", "Mozilla/5.0 (Linux; rv:78.0) Gecko/20100101 Firefox/78.0");
 
 user_pref("privacy.firstparty.isolate", false); // breaks too much stuff for now :(
+user_pref("browser.display.use_document_fonts", 1); // why do they disable this lol
+
+user_pref("privacy.clearOnShutdown.cache", false);
+user_pref("privacy.clearOnShutdown.cookies", false);
+user_pref("privacy.clearOnShutdown.downloads", false);
+user_pref("privacy.clearOnShutdown.formdata", false);
+user_pref("privacy.clearOnShutdown.history", false);
+user_pref("privacy.clearOnShutdown.offlineApps", false);
+user_pref("privacy.clearOnShutdown.openWindows", false);
+user_pref("privacy.clearOnShutdown.sessions", false);
+user_pref("privacy.clearOnShutdown.siteSettings", false);
