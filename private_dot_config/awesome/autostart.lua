78eb2b8e04ece4b009c85cf2b48c0d57e3a80eda
AAGaming <aa@bigdumb.gq>
2021-12-28 19:49:08 -0500

    Update private_dot_config/awesome/autostart.lua Update private_dot_config/awesome/rc.lua

--- private_dot_config/awesome/autostart.lua
+++ private_dot_config/awesome/autostart.lua
@@ -1,5 +1,4 @@
 local awful = require("awful")
-local gears = require("gears")
 
 local autostarts =
 {
--- private_dot_config/awesome/rc.lua
+++ private_dot_config/awesome/rc.lua
@@ -548,6 +548,15 @@ client.connect_signal("mouse::enter", function(c)
     c:emit_signal("request::activate", "mouse_enter", {raise = false})
 end)
 
-client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
+client.connect_signal("focus",
+        function(c)
+                if c.maximized_horizontal == true and c.maximized_vertical == true then
+                        c.border_width = "0"
+                        c.border_color = beautiful.border_focus
+                else
+                        c.border_width = beautiful.border_width
+                        c.border_color = beautiful.border_focus
+                end
+        end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
