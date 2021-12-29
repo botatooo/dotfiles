efd290492dd40a44f64d76f5feacf1e7a38f4e3b
AAGaming <aa@bigdumb.gq>
2021-12-28 20:00:57 -0500

    Update private_dot_config/awesome/rc.lua

--- private_dot_config/awesome/rc.lua
+++ private_dot_config/awesome/rc.lua
@@ -119,7 +119,7 @@ mykeyboardlayout = awful.widget.keyboardlayout()
 
 -- {{{ Wibar
 -- Create a textclock widget
-mytextclock = wibox.widget.textclock()
+mytextclock = wibox.widget.textclock(" %a %b %d %l:%M%p ", 15)
 
 -- Create a wibox for each screen and add it
 local taglist_buttons = gears.table.join(
