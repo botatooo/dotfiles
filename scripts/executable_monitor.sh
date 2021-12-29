379db9dcff91cebac4f271d7b915f6bcc003cb8e
AAGaming <aa@bigdumb.gq>
2021-12-29 17:49:27 -0500

    Update private_dot_config/polybar/config Update scripts/executable_monitor.sh

--- scripts/executable_monitor.sh
+++ scripts/executable_monitor.sh
@@ -6,6 +6,13 @@ case ${MONS_NUMBER} in
     2)
         mons -e right
         ;;
+    # 3)
+    #     xrandr \
+    #         --fb 4566x2648 \
+    #         --output eDP --primary --mode 1920x1080 --rate 144 --pos 0x0 --rotate normal \
+    #         --output HDMI-1-0 --mode 1366x768 --right-of eDP --rotate normal \
+    #         --output Virtual-2-1 --mode 1280x800 --right-of HDMI-1-0 --rotate normal
+    #     ;;
     *)
         # Handle it manually
         ;;
