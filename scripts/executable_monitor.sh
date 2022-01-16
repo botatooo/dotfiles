f625971d48bf9374d1004f7c0c17755c19d414d5
AAGaming <aa@bigdumb.gq>
2022-01-16 14:15:35 -0500

    Update private_dot_config/i3/config Update scripts/executable_monitor.sh

--- scripts/executable_monitor.sh
+++ scripts/executable_monitor.sh
@@ -1,25 +1,13 @@
 #!/bin/sh
-i=0
-for status in /sys/class/drm/*/status; do
-    [ "$(<"$status")" = 'connected' ] && i=$((i+1))
-done
-case ${i} in
-    1)
-        mons -o
-        ;;
-    2)
-        mons -e right
-        ;;
-    # 3)
-    #     xrandr \
-    #         --fb 4566x2648 \
-    #         --output eDP --primary --mode 1920x1080 --rate 144 --pos 0x0 --rotate normal \
-    #         --output HDMI-1-0 --mode 1366x768 --right-of eDP --rotate normal \
-    #         --output Virtual-2-1 --mode 1280x800 --right-of HDMI-1-0 --rotate normal
-    #     ;;
-    *)
-        # Handle it manually
-        ;;
-esac
+
+function isHDMIConnected {
+	! xrandr | grep "^HDMI" | grep disconnected
+}
+
+if isHDMIConnected; then
+mons -e right
+else
+mons -o
+fi
 
 feh --bg-scale /usr/share/wallpapers/MilkyWay/contents/images/5120x2880.png
